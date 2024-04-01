024a61beab78da20ef829306c022de3fb97a6806
Marius <mariuseco15@yahoo.com>
2024-04-01 23:21:40 +0300

    no errors please

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <limits>
 
 class RandWord {
 public:
     RandWord() = default;
-
+    /// \brief Selects a random word from a list
+    /// \return a random word
     static std::string getRandomWord() {
         const std::string wordList[4] = {"apple", "banana", "kiwi", "mango"};
         std::random_device rd;
@@ -14,6 +16,7 @@ public:
         int index = distribution(rng);
         return wordList[index];
     }
+
 };
 
 class Player {
@@ -34,17 +37,6 @@ public:
         return os;
     }
 
-//    const std::string &getName() const {
-//        return name;
-//    }
-//
-//    void increaseScore(int points) {
-//        score += points;
-//    }
-//
-//    int getScore() const {
-//        return score;
-//    }
 };
 
 class Scoreboard {
@@ -72,24 +64,34 @@ public:
         return os;
     }
 
+    /// \brief Displays a welcome message and starts the game
     static void beginGame() {
         std::cout << "Welcome to the Hangman game!" << "\n";
         std::cout << "Press enter to continue..." << "\n";
-        std::cin.ignore();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
     };
 
+    /// \brief Displays the word that you are guessing
+    /// \param word constant reference to a string object that contains the word that you are guessing
     static void displayWord(const std::string &word) {
         std::cout << "Word to guess: " << word << "\n";
     };
 
+    /// \brief Displays a typed message
+    /// \param message constant reference to a string object that contains a typed message
     static void displayMessage(const std::string &message) {
         std::cout << message << "\n";
     }
 
+    /// \brief Displays the number of tries remaining
+    /// \param tries integer variable that contains the number of tries remaining
     static void displayTries(int tries) {
         std::cout << "Tries remaining: " << tries << "\n";
     }
 
+    /// \brief Displays if the player has won or lost
+    /// \param is_winner bool variable that determines the win/lose state
+    /// \param secret constant reference to a string object that contains the word that we need to guess
     static void displayEnd(bool is_winner, const std::string &secret) {
         if (is_winner)
             std::cout << "Congratulations! You guessed the word: " << secret << "\n";
@@ -102,8 +104,11 @@ private:
     Scoreboard scoreboard;
     UI ui;
     Player player;
+    /// \brief Number of incorrect guesses
     int incorrectGuesses;
+    /// \brief The letters that have been guessed so far
     std::string guessedLetters;
+    /// \brief The maximum number of tries
     const int MAX_TRIES;
 public:
     HangmanGame(const Scoreboard &scoreboard, const UI &ui, const Player &player,
@@ -118,6 +123,9 @@ public:
                                                                                                             MAX_TRIES(
                                                                                                                     maxTries) {}
 
+    /// \brief Decides if a character is a letter
+    /// \param letter constant reference to a character
+    /// \return true if the character is a letter, false otherwise
     static bool not_letter(const char& letter) {
         if ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z')) {
             return true;
@@ -125,10 +133,30 @@ public:
             return false;
     }
 
+    /// \brief Updates the word that we have to guess based on if the letter guessed is in the word or not
+    /// \param secret the word that we have to guess
+    /// \param letter the letter that has been guessed
+    /// \param word_to_guess the terminal display of the word that we have to guess
+    /// \return a bool variable which represents if the letter that has been guessed was in the word or not
+    static int addLetters(const std::string& secret, const char& letter, std::string& word_to_guess){
+        bool search = false;
+        for (int i = 0; i < int(secret.length()); ++i) {
+            if (secret[i] == letter) {
+                word_to_guess[i] = letter;
+                search = true;
+            }
+        }
+        return search;
+    }
+
+    /// \brief Main game function that runs the proper game code
     void game() {
+        /// \brief The word that we have to guess
         std::string secret = RandWord::getRandomWord();
+        /// \brief A simple terminal display of the word we have to guess
         std::string word_to_guess(secret.length(), '*');
         UI::beginGame();
+        /// \brief Condition for the game to continue
         while (incorrectGuesses < MAX_TRIES && word_to_guess != secret) {
 
             UI::displayWord(word_to_guess);
@@ -148,15 +176,9 @@ public:
                 UI::displayTries(MAX_TRIES - incorrectGuesses);
                 continue;
             }
-            guessedLetters += letter;
-            bool search = false;
-            for (int i = 0; i < int(secret.length()); ++i) {
-                if (secret[i] == letter) {
-                    word_to_guess[i] = letter;
-                    search = true;
-                }
-            }
 
+            guessedLetters += letter;
+            bool search=addLetters(secret,letter,word_to_guess);
             if (!search) {
                 incorrectGuesses++;
                 UI::displayMessage("Unfortunately the letter " + std::string(1, letter) +
